76d8431c6b6e0e3ac7347cb8a8be0053f29a83d1
zacharylayne <zacharylayne@hotmail.com>
2024-08-15 04:32:17 -0700

    New icons & images; removing unused files.
    Adding new icons & images. Removed some files that were no longer being referenced by a project.

--- Source/ConstTypeArgs.Delegates/Base.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConstTypeArgs.Core;
-using ConstTypeArgs.Delegates.Actions;
-
-#pragma warning disable IDE1006 // Naming Styles
-
-namespace ConstTypeArgs.Delegates.Funcs;
-
-public interface K_DelegateArray<out T>
-    : K_Array<Delegate>;
